9a99685ec9bf162284a5881525ef6a24235715e7
web-ljq <ljqrongyao@outlook.com>
2021-11-26 23:02:24 +0800

    fix

--- miniprogram/pages/stylesm/index.ts
+++ miniprogram/pages/stylesm/index.ts
@@ -8,73 +8,81 @@ Page({
       {
         name: "基础组件",
         des: "Basic",
-        icon: "",
+        icon: "icon-basic",
+        path: "basic"
       },
       {
         name: "布局组件",
         des: "Composition",
-        icon: "",
+        icon: "icon-composition",
+        path: "composition"
       },
       {
         name: "视图组件",
         des: "View",
-        icon: "",
+        icon: "icon-view",
       },
       {
         name: "导航组件",
         des: "Navigation",
-        icon: "",
+        icon: "icon-navigation",
       },
       {
         name: "交互组件",
         des: "Interactive",
-        icon: "",
+        icon: "icon-interactive",
       },
       {
         name: "表单组件",
         des: "Form",
-        icon: "",
+        icon: "icon-form",
       },
     ],
+  },
+
+  navigate: function (e: any) {
+    wx.navigateTo({
+      url: `/pages/stylesm/${e.currentTarget.dataset.path}/index`
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad() {},
+  onLoad() { },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {},
+  onReady() { },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {},
+  onShow() { },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {},
+  onHide() { },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {},
+  onUnload() { },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {},
+  onPullDownRefresh() { },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {},
+  onReachBottom() { },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {},
+  onShareAppMessage() { },
 });
