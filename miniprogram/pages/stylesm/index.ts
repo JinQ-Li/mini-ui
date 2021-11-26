c7d34c5e8c6ffe8cc70797fd532f76debbc24f61
孑然一身 <ljqrongyao@outlook.com>
2021-11-26 16:56:15 +0800

    style

---
+++ miniprogram/pages/stylesm/index.ts
@@ -0,0 +1,80 @@
+// pages/stylesm/index.ts
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list: [
+      {
+        name: "基础组件",
+        des: "Basic",
+        icon: "",
+      },
+      {
+        name: "布局组件",
+        des: "Composition",
+        icon: "",
+      },
+      {
+        name: "视图组件",
+        des: "View",
+        icon: "",
+      },
+      {
+        name: "导航组件",
+        des: "Navigation",
+        icon: "",
+      },
+      {
+        name: "交互组件",
+        des: "Interactive",
+        icon: "",
+      },
+      {
+        name: "表单组件",
+        des: "Form",
+        icon: "",
+      },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {},
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {},
+});
